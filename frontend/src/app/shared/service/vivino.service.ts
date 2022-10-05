72f8070531f02384bce4427701e1bce3b48dcd26
Anton Isaev <aisaev@intetics.com>
2022-10-05 14:47:39 +0300

    Changed backend url

--- frontend/src/app/shared/service/vivino.service.ts
+++ frontend/src/app/shared/service/vivino.service.ts
@@ -4,7 +4,7 @@ import { catchError, map } from 'rxjs/operators';
 import { IWineRating } from '../model/wine-rating.model';
 
 export class VivinoService {
-  private resourceUrl = 'https://find-your-wine.herokuapp.com/api/wine';
+  private resourceUrl = 'https://find-your-wine.pp.ua/api/wine';
 
   public getWineRating(wineName: string): Observable<IWineRating | undefined> {
     return ajax.get(`${this.resourceUrl}/rating?name=${wineName}`).pipe(
