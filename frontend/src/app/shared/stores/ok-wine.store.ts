3e8c5ee100c747384165f0f514a52d409ec14810
Anton Isaiev <aisaiev@aisaiev.net>
2024-07-26 16:47:58 +0300

    Fixed okwine store

--- frontend/src/app/shared/stores/ok-wine.store.ts
+++ frontend/src/app/shared/stores/ok-wine.store.ts
@@ -31,18 +31,19 @@ export const addOkWineWineRating = (): void => {
 };
 
 const getWineListItems = (): Element[] => {
-  return [...Array.from(document.querySelectorAll('.sc-1e370631-0.iRjZwN'))];
+  return [...Array.from(document.querySelectorAll('[data-testid="integrationProductContainer"]'))];
 };
 
 const getRating = (wineItem: Element): Observable<IWineRating> => {
-  const wineTitle = wineItem.querySelector('.sc-1e370631-11.yyDwN').textContent;
+  const links = wineItem.querySelectorAll('a');
+  const wineTitle = links.item(links.length - 1).firstChild.textContent;
   const wineName = getWineName(wineTitle as string);
   return wineName ? getWineRating(wineName) : EMPTY;
 };
 
 const addRating = (wineItem: Element, wineRating: IWineRating): void => {
   if (!wineItem.querySelector(`.${VivinoBadgeRatingClass}`)) {
-    const item = wineItem.querySelector('.sc-1e370631-1.fbwtWi');
+    const item = wineItem.firstChild.firstChild;
     const wineRatingBadge = createOkWineWineRatingBadge(wineRating);
     item.appendChild(wineRatingBadge);
   }
@@ -54,48 +55,44 @@ const getWineName = (wineTitle: string): string => {
   const tempWineTitle = wineTitle.split(' / ')[1];
   if (tempWineTitle) wineTitle = tempWineTitle;
 
-  wineTitle = wineTitle.toLowerCase();
-
-  wineTitle = wineTitle.replace('вино', '');
-
-  wineTitle = wineTitle.replace('ігристе', '');
-  wineTitle = wineTitle.replace('игристое', '');
-  wineTitle = wineTitle.replace('sparkling', '');
-
-  wineTitle = wineTitle.replace('червоне', '');
-  wineTitle = wineTitle.replace('червоний', '');
-  wineTitle = wineTitle.replace('красное', '');
-  wineTitle = wineTitle.replace('біле', '');
-  wineTitle = wineTitle.replace('білий', '');
-  wineTitle = wineTitle.replace('рожеве', '');
-  wineTitle = wineTitle.replace('рожеве', '');
-  wineTitle = wineTitle.replace('рожевий', '');
-  wineTitle = wineTitle.replace('розовое', '');
-  wineTitle = wineTitle.replace('коричневий', '');
-
-  wineTitle = wineTitle.replace('напівсухе', '');
-  wineTitle = wineTitle.replace('полусухое', 'semi-dry');
-  wineTitle = wineTitle.replace('сухе', '');
-  wineTitle = wineTitle.replace('сухое', '');
-  wineTitle = wineTitle.replace('напівсолодке', '');
-  wineTitle = wineTitle.replace('полусладкое ', '');
-  wineTitle = wineTitle.replace('солодкий', '');
-  wineTitle = wineTitle.replace('десертне', '');
-  wineTitle = wineTitle.replace('десертное ', '');
-  wineTitle = wineTitle.replace('кріплене', '');
-  wineTitle = wineTitle.replace('крепленое', '');
-  wineTitle = wineTitle.replace('брют', '');
-  wineTitle = wineTitle.replace('екстра', '');
-
-  wineTitle = wineTitle.replace('шираз', 'shiraz');
-  wineTitle = wineTitle.replace('шардоне', 'chardonnay');
-  wineTitle = wineTitle.replace('каберне', 'cabernet');
-  wineTitle = wineTitle.replace('совіньон', 'sauvignon');
-  wineTitle = wineTitle.replace('совиньон', 'sauvignon');
-  wineTitle = wineTitle.replace('пино', 'pinot');
-  wineTitle = wineTitle.replace('піно', 'pinot');
-  wineTitle = wineTitle.replace('гри', 'grigio');
-  wineTitle = wineTitle.replace('грі', 'grigio');
+  wineTitle = wineTitle
+    .toLowerCase()
+    .replace('вино', '')
+    .replace('ігристе', '')
+    .replace('игристое', '')
+    .replace('sparkling', '')
+    .replace('червоне', '')
+    .replace('червоний', '')
+    .replace('красное', '')
+    .replace('біле', '')
+    .replace('білий', '')
+    .replace('рожеве', '')
+    .replace('рожеве', '')
+    .replace('рожевий', '')
+    .replace('розовое', '')
+    .replace('коричневий', '')
+    .replace('напівсухе', '')
+    .replace('полусухое', 'semi-dry')
+    .replace('сухе', '')
+    .replace('сухое', '')
+    .replace('напівсолодке', '')
+    .replace('полусладкое ', '')
+    .replace('солодкий', '')
+    .replace('десертне', '')
+    .replace('десертное ', '')
+    .replace('кріплене', '')
+    .replace('крепленое', '')
+    .replace('брют', '')
+    .replace('екстра', '')
+    .replace('шираз', 'shiraz')
+    .replace('шардоне', 'chardonnay')
+    .replace('каберне', 'cabernet')
+    .replace('совіньон', 'sauvignon')
+    .replace('совиньон', 'sauvignon')
+    .replace('пино', 'pinot')
+    .replace('піно', 'pinot')
+    .replace('гри', 'grigio')
+    .replace('грі', 'grigio');
 
   const wineName = wineTitle.trim().match(/([a-z]{2,})|([а-яґєії]{2,})/g);
 
--- frontend/src/app/shared/utils/store.util.ts
+++ frontend/src/app/shared/utils/store.util.ts
@@ -41,7 +41,10 @@ export const isGoodWineWineDepartment = (): boolean => {
 export const isOkWineWineDepartment = (): boolean => {
   return (
     window.location.host.includes(Host.OkWine) &&
-    (window.location.pathname.includes('vina') || window.location.pathname.includes('vino') || window.location.pathname.includes('liker'))
+    (window.location.pathname.includes('vina') ||
+      window.location.pathname.includes('vino') ||
+      window.location.pathname.includes('liker') ||
+      window.location.pathname.includes('-wine'))
   );
 };
 
