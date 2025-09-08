6fe7e56777400491997100c11d8e2118764428d0
Anton Isaiev <aisaiev@aisaiev.net>
2025-09-08 12:51:35 +0300

    Migrate backend from nestjs to hono

--- backend/src/app.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { WineModule } from './wine/wine.module';
-import { VivinoModule } from './vivino/vivino.module';
-
-@Module({
-  imports: [WineModule, VivinoModule],
-})
-export class AppModule {}
